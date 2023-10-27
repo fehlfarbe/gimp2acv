a29c846bfb3aabd0f86381118ffa95af41c72a6b
fehlfarbe <marcus@degenkolbe.eu>
2023-10-27 18:17:41 +0200

    fixes #2 Added better interpolation of Photoshop's Lagrange curve

--- acv.py
+++ acv.py
@@ -5,6 +5,8 @@ import struct
 from typing import AnyStr
 
 import numpy as np
+from numpy.polynomial.polynomial import Polynomial
+from scipy.interpolate import lagrange
 
 
 def readInt16(array: AnyStr, start: int, big_endian: bool = False) -> int:
@@ -22,7 +24,7 @@ class ACVCurve:
 
     def __init__(self, version: int = 4, curves: list[list[(int, int)]] | None = None):
         self._version = version
-        if not curves:
+        if curves is None:
             self._curves: list[list[(int, int)]] = []
         else:
             self._curves: list[list[(int, int)]] = curves
@@ -40,9 +42,9 @@ class ACVCurve:
                 curve = []
                 point_count = readInt16(data, 0x4 + offset, True)
                 for p in range(point_count):
-                    a = readInt16(data, 0x4 + offset + 0x2 + p * 0x4, True)
-                    b = readInt16(data, 0x4 + offset + 0x4 + p * 0x4, True)
-                    curve.append([a, b])
+                    y = readInt16(data, 0x4 + offset + 0x2 + p * 0x4, True)
+                    x = readInt16(data, 0x4 + offset + 0x4 + p * 0x4, True)
+                    curve.append([x, y])
                 offset += 0x2 + point_count * 4
                 curves.append(curve)
 
@@ -73,31 +75,39 @@ class ACVCurve:
             for line in fp.readlines():
                 line = line.lstrip().replace(')', '').replace('\n', '')
                 if line.startswith('(samples'):
-                    curve = []
-                    values = line.split(' ')[2:]
-                    # y_values = [float(v)*255 for v in values]
-                    y_values = np.array(values)
-                    y_values = y_values.astype(np.float64)
-                    x_values = np.arange(0, 1, 1 / 256.0)
-                    z = np.polyfit(x_values, y_values, 5)
-                    filepointer = np.poly1d(z)
-                    x_new = np.linspace(x_values[0], x_values[-1], 16)
-                    y_new = filepointer(x_new)
-                    # plt.plot(x_values, y_values, 'g')
-                    # plt.scatter(x_new, y_new, marker='o', color='b')
-                    # plt.show()
-
-                    for x, y in zip(x_new, y_new):
-                        y, x = int(np.round(x * 255)), int(np.round(y * 255))
-                        y = max(y, 0)
-                        x = max(x, 0)
-                        x = min(x, 255)
-                        y = min(y, 255)
-                        curve.append([x, y])
-                    # plt.scatter([v[1] for v in curve], [v[0] for v in curve], marker='o', color='r')
-                    # plt.show()
+                    # read all samples and convert to 0-255 range
+                    samples = np.array(line.split(' ')[2:]).astype(np.float64) * 255
+                    # interpolate lagrange polynom and extract the important points
+                    curve = cls._interpolateLagrangeFromSamples(samples)
+                # extract original points
+                # if line.startswith("(points"):
+                #     curve = []
+                #     values = line.split(' ')[2:]
+                #     for x, y in zip(values[0::2], values[1::2]):
+                #         x, y = int(np.round(float(x) * 255)), int(np.round(float(y) * 255))
+                #         curve.append((y, x))
                     curves.append(curve)
         return cls(curves=curves)
+
+    @staticmethod
+    def _interpolateLagrangeFromSamples(samples: np.array, numberOfPoints: int = 16) -> list[tuple[int, int]]:
+        x_values = np.arange(0, len(samples))
+        # fit a polynom that approximates the samples
+        fittedPolynom = Polynomial.fit(x_values, samples, 21)
+        # get points on fittedPolynom for chebyshev nodes
+        def chebyshev_nodes(a, b, n):
+            nodes = [(a + b) / 2 + ((b - a) / 2) * np.cos((2 * k - 1) * np.pi / (2 * n)) for k in range(1, n + 1)]
+            return np.array(sorted(nodes))
+        chebyshevX = chebyshev_nodes(0, x_values[-1], numberOfPoints)
+        chebyshevY = fittedPolynom(chebyshevX)
+        # calculate lagrange polynom and extract y values for chebyshev nodes
+        lagrangePoly = lagrange(chebyshevX, chebyshevY)
+        importance = np.abs(lagrangePoly.coeffs)
+        indices = np.sort(np.argsort(importance)[-numberOfPoints:])
+        lagrangePoints = lagrangePoly(chebyshevX[indices])
+        # export values for chebyshev nodes and cast them to int
+        lagrangePoints = [(int(x), int(y)) for x, y in zip(chebyshevX[indices], lagrangePoints)]
+        return lagrangePoints
 
     @classmethod
     def fromOldGIMPFormat(cls, filename: str) -> ACVCurve:
@@ -114,8 +124,7 @@ class ACVCurve:
                 for i in range(0, len(points) - 1, 2):
                     # ignore points with -1 value
                     if points[i] != -1:
-                        # x,y swapped?
-                        curve.append([points[i + 1], points[i]])
+                        curve.append([points[i], points[i + 1]])
                 if len(curve) > 16:
                     print(f"Warning: {len(curve)} points in curve! Photoshop point limit is <= 16 points.")
                 curves.append(curve)
@@ -131,6 +140,17 @@ class ACVCurve:
     def getCurves(self) -> list[list[(int, int)]]:
         return self._curves
 
+    def plot(self) -> None:
+        import matplotlib.pyplot as plt
+
+        for curve in self._curves:
+            points = np.array([(x, y) for y, x in curve])
+            plt.plot(points[:, 0], points[:, 1])
+            plt.scatter(points[:, 0], points[:, 1], marker='o')
+        plt.xlim(0, 255)
+        plt.ylim(0, 255)
+        plt.show()
+
     def __str__(self):
         return f"<ACVCurve version={self._version}, curves={self.curveCount}>"
 
@@ -145,9 +165,9 @@ class ACVCurve:
                 # count of points in the curve
                 f.write(struct.pack('>h', len(c)))
                 # curve points
-                for v in c:
-                    f.write(struct.pack('>h', v[0]))
-                    f.write(struct.pack('>h', v[1]))
+                for x, y in c:
+                    f.write(struct.pack('>h', y))
+                    f.write(struct.pack('>h', x))
 
     def __eq__(self, other: ACVCurve) -> bool:
         for c1, c2 in zip(self._curves, other.getCurves()):
--- test/gimp2acvTest.py
+++ test/gimp2acvTest.py
@@ -6,12 +6,18 @@ class Gimp2AcvTest(unittest.TestCase):
 
     def testLoadSaveLoad(self):
         acvFile = "/tmp/curve.acv"
-        curve1 = ACVCurve.fromGIMPCurve("../res/testcurves")
+        curve1 = ACVCurve.fromGIMPCurve("../res/testcurves.gcv")
         curve1.toFile(acvFile)
         curve2 = ACVCurve.fromACVFile(acvFile)
 
         self.assertEqual(curve1.curveCount, curve2.curveCount)
         self.assertEqual(curve1, curve2)
+
+    # def testLoadGCVAndPlot(self):
+    #     gcvFile = "../res/arthurrackham.gcv"
+    #     curve = ACVCurve.fromGIMPCurve(gcvFile)
+    #     curve.plot()
+    #     curve.toFile(f"{gcvFile}.acv")
 
 
 if __name__ == '__main__':
